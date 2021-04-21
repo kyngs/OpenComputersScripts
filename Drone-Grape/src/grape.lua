232568f64de55fa5c8eb4f9a4e7f9f3388744242
kyngs <snezbeda@gmail.com>
2021-04-21 20:38:54 +0200

    Update

--- Drone-Grape/src/grape.lua
+++ Drone-Grape/src/grape.lua
@@ -1,14 +1,24 @@
-function run()
-    local waypoints = navigation.findWaypoints(256)
-    for i = 1, waypoints.n do
-        local waypoint = waypoints[i]
-        if waypoint.label == "grape_dock" then
-            local pos = waypoint.position
-            status("-> Base")
-            drone.move(pos[1], pos[2], pos[3])
-            status("LOOOL")
-        end
+--function run()
+--    local waypoints = navigation.findWaypoints(256)
+--    for i = 1, waypoints.n do
+--        local waypoint = waypoints[i]
+--        if waypoint.label == "grape_dock" then
+--            local pos = waypoint.position
+--            status("-> Base")
+--            drone.move(pos[1], pos[2], pos[3])
+--            status("LOOOL")
+--        end
+--    end
+--end
+--
+--register_coroutine(run, {})
+local waypoints = navigation.findWaypoints(256)
+for i = 1, waypoints.n do
+    local waypoint = waypoints[i]
+    if waypoint.label == "grape_dock" then
+        local pos = waypoint.position
+        status("-> Base")
+        drone.move(pos[1], pos[2], pos[3])
+        status("LOOOL")
     end
 end
-
-register_coroutine(run, {})
