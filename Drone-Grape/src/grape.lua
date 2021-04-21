32ca74bb43e39f9fa55e780e87655ccfd94ca4de
kyngs <snezbeda@gmail.com>
2021-04-21 20:17:59 +0200

    Update

--- Drone-Grape/src/grape.lua
+++ Drone-Grape/src/grape.lua
@@ -1,5 +1,13 @@
-for k, v in pairs(navigation.findWaypoints(256)) do
-    if v["label"] == "grape_dock" then
-        status(v["position"][2])
+function run()
+    for k, v in pairs(navigation.findWaypoints(256)) do
+        if not v then
+            goto continue
+        end
+        if v["label"] == "grape_dock" then
+            status(v["position"][2])
+        end
+        ::continue::
     end
 end
+
+register_coroutine(run, {})
