fe42b638233221a27e7c2074c3ad026cb807e837
kyngs <snezbeda@gmail.com>
2021-04-21 17:29:07 +0200

    Update

--- Drone-Core/src/utils.lua
+++ Drone-Core/src/utils.lua
@@ -1,11 +1,7 @@
 function run()
-    status("TEST")
-    sleep(2)
     while (true) do
         computer.beep(100)
         sleep(1)
     end
 end
-status("TEST")
-sleep(2)
 register_coroutine(run, {})
