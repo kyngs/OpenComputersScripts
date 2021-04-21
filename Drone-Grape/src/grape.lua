902ca5c4b8be3a2647a534e772d3962d498f1f31
kyngs <snezbeda@gmail.com>
2021-04-21 19:43:33 +0200

    Update

--- Drone-Grape/src/grape.lua
+++ Drone-Grape/src/grape.lua
@@ -1,5 +1,5 @@
 for k, v in pairs(navigation.findWaypoints(256)) do
-    if k["label"] == "grape_dock" then
-        status(k["position"][2])
+    if v["label"] == "grape_dock" then
+        status(v["position"][2])
     end
 end
