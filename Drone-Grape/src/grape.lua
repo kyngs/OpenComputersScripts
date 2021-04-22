8f129b368fdda9962644eca0bda423257022e287
kyngs <snezbeda@gmail.com>
2021-04-22 14:43:43 +0200

    Update

--- Drone-Grape/src/grape.lua
+++ Drone-Grape/src/grape.lua
@@ -28,7 +28,7 @@ function recalculate_waypoints()
 
         if starts_with(label,"grape_waypoint_") then
             local str, _ = label:gsub("grape_waypoint_", "");
-            status(tostring(tonumber(str)))
+            status(tostring(converted_pos))
             --temp_rows:insert(tonumber(str), converted_pos)
         end
 
