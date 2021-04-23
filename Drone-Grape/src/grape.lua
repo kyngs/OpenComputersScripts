1b55e3592adf41cfa3571ce17924d14a57351684
kyngs <snezbeda@gmail.com>
2021-04-23 20:20:39 +0200

    Test

--- Drone-Grape/src/grape.lua
+++ Drone-Grape/src/grape.lua
@@ -46,4 +46,33 @@ function dock()
     status("Docked")
 end
 
+function travel_to_waypoint(index)
+    status("Traveling")
+    calculate_waypoints()
+    local waypoint = WAYPOINTS[index];
+    drone.move(waypoint.x, waypoint.y, waypoint.z)
+    repeat until is_at(WAYPOINTS[index])
+    sleep(1)
+end
+
 dock()
+
+while true do
+    if (is_at(DOCK)) then
+        if (get_total_inventory_space_occupied() <= 0) and (get_charge_percent() >= 95) then
+            goto stop_dock
+        end
+    else
+        if (get_total_inventory_space_remaining() == 0) or (get_charge_percent() <= 10) then
+            status("oh no")
+            goto continue
+        end
+    end
+
+    ::stop_dock::
+
+    travel_to_waypoint(start_waypoint_index)
+    start_waypoint_index = start_waypoint_index + 1
+
+    ::continue::
+end
